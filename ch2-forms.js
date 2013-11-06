be7d12007e37b4dcb8c69ddb995188e55bfff239
Lana Zwart <lanazwart@gmail.com>
2013-11-06 15:15:44 +0000

    Update ch2-forms.js

--- ch2-forms.js
+++ ch2-forms.js
@@ -8,7 +8,8 @@ $(function() {
 		var inputs = $('input[name], select[name]', this);
 		for (var i = 0; i < inputs.length; i++)
 		{
-			console.log(inputs[i].val());
+			var tmp = $(inputs[i]).val();
+			console.log(tmp);
 		}
 		
 		//could use this in future to post to server
