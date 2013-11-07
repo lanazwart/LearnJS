b41aeb42149741c770e5d3bcedcc0488bbda8b53
Lana Zwart <lanazwart@gmail.com>
2013-11-07 12:20:42 +0000

    Update ch2-forms.js

--- ch2-forms.js
+++ ch2-forms.js
@@ -1,21 +1,48 @@
+/Selecting Speeds:
+// 1. ~id
+// 2. .class
+// 3. by attr (name=something)
+//
+//
 $(function() {
-
-	$('#user-form').submit(function(evt) {
+		
+		$('input[name=balloon]').hide();
+		
+		$('input[name=name]').val('Jesse');
+		$('input[name=age]').val('50');
+		$('select[name=gender] option[value=f]').attr('selected', true);
+		
+		$('select[name=gender]').change(function(evt) {
+			evt.preventDefault();
+			var selected = $(this).val();
+			if (selected == 'f') {
+				$('input[name=balloon]').show();
+				
+			}  else {
+				$('input[name=balloon]').hide();
+			}	
+		});
+		
+		$('#user-form').submit(function(evt) {
 		evt.preventDefault();
-		
-		//$(this).hide();
-		
-		var inputs = $('input[name], select[name]', this);
-		for (var i = 0; i < inputs.length; i++)
-		{
-			var tmp = $(inputs[i]).val();
-			console.log(tmp);
-		}
-		
-		//could use this in future to post to server
-		//var post_data = $(this).serialize();
-		//console.log(post_data);
-		
-	});
+		//var self this;		
+			var inputs = $('input[name], select[name]', this);
+			for (var i = 0; i < inputs.length; i++)
+			{
+				var tmp = $(inputs[i]).val();
+				//console.log(tmp);
+			}
+			
+			$.each(inputs, function(key, input) {
+				console.log(key);
+				var tmp = $(input).val();
+				console.log(tmp);
+			});
+			
+			//could use this in future to post to server
+			//var post_data = $(this).serialize();
+			//console.log(post_data);
+			
+		});
 
 });
